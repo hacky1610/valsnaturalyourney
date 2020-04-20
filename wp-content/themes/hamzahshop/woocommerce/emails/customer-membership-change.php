86c4e2b7b1ef5497fa1cc06f61623ada9e5fe494
Hacky <hacky1610@gmx.de>
2020-04-20 21:23:33 +0200

    Restyle mails

--- wp-content/themes/hamzahshop/woocommerce/emails/customer-membership-change.php
+++ wp-content/themes/hamzahshop/woocommerce/emails/customer-membership-change.php
@@ -5,10 +5,10 @@ defined( 'ABSPATH' ) || exit;
 do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
 <p><?php printf( esc_html__( 'Hi %s,', 'woocommerce' ), esc_html( $first_name) ); ?></p>
-<p>Die Zugangsmethode zum Kurs BOOST LA POUSSE DE TES CHEVEUX hat sich geändert. 
-Ab jetzt können sie sich über diese Adresse einloggen: <?php echo make_clickable( esc_url( wc_get_page_permalink( 'myaccount' ) ) ) ;?></p>
-<?php
-printf( 
-    'Dein Username ist %1$s', 
-    '<strong>' . esc_html( $user_login ) . '</strong>');
+<?php 
+echo "Die Zugangsmethode zum Kurs <strong>BOOST LA POUSSE DE TES CHEVEUX</strong> hat sich geändert. 
+Ab jetzt müssen sie sich über diese Adresse einloggen: " . make_clickable( esc_url( wc_get_page_permalink( 'myaccount' ) ) ) . ". </br>";
+echo  'Dein Username ist <strong>' . esc_html( $user_login ) . '</strong>. ';
+echo "Wenn du dein Paswort vergessen hast, dann kannst du es <a href='" . esc_url($user_login_uri ) .  "' >hier</a> zurücksetzen";
+echo "<p>Liebe Grüße Valérie</p>";
 do_action( 'woocommerce_email_footer', $email );
--- wp-content/themes/hamzahshop/woocommerce/emails/customer-registration.php
+++ wp-content/themes/hamzahshop/woocommerce/emails/customer-registration.php
@@ -5,10 +5,13 @@ defined( 'ABSPATH' ) || exit;
 do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
 <p><?php printf( esc_html__( 'Hi %s,', 'woocommerce' ), esc_html( $first_name) ); ?></p>
-<p><?php printf( 
-    'Die Zugangsmethode zum Kurs BOOST LA POUSSE DE TES CHEVEUX hat sich geändert. Wir haben für dich ein neues Konto eingerichtet dein Username ist %1$s. Über diesen Link kannst du dich einloggen: %2$s', 
-    '<strong>' . esc_html( $user_login ) . '</strong>',
-     make_clickable( esc_url($user_login_uri ) ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+<p><?php echo  
+    "Die Zugangsmethode zum Kurs <strong>BOOST LA POUSSE DE TES CHEVEUX</strong> hat sich geändert. 
+    Wir haben für dich ein neues Konto eingerichtet. Dein Username ist <strong>" . esc_html( $user_login ) . "</strong>. 
+    Du musst jetzt nur noch dein Passwort ändern. Dafür musst du  <a href='" . esc_url($user_login_uri ) .  "' >hier</a> clicken.
+    Danach kannst du dich einloggen um dein Kurs zu sehen "
+     ?></p>
+     <p>Liebe Grüße Valérie</p>
 <?php
 
 do_action( 'woocommerce_email_footer', $email );
