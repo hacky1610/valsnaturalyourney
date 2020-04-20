9607c7731c9e21a364f2c99e685e0ad598fe3c50
Hacky <hacky1610@gmx.de>
2020-04-20 20:06:15 +0200

    Add user name

--- wp-content/themes/hamzahshop/woocommerce/emails/customer-membership-change.php
+++ wp-content/themes/hamzahshop/woocommerce/emails/customer-membership-change.php
@@ -7,7 +7,8 @@ do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 <p><?php printf( esc_html__( 'Hi %s,', 'woocommerce' ), esc_html( $first_name) ); ?></p>
 <p>Die Zugangsmethode zum Kurs BOOST LA POUSSE DE TES CHEVEUX hat sich geändert. 
 Ab jetzt können sie sich über diese Adresse einloggen: <?php echo make_clickable( esc_url( wc_get_page_permalink( 'myaccount' ) ) ) ;?></p>
-
 <?php
-
+printf( 
+    'Dein Username ist %1$s', 
+    '<strong>' . esc_html( $user_login ) . '</strong>');
 do_action( 'woocommerce_email_footer', $email );
