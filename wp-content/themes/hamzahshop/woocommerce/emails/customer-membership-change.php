636f22f282169416c7147d89ba2832738fb9948e
Hacky <hacky1610@gmx.de>
2020-04-23 21:07:29 +0200

    add translation

--- wp-content/themes/hamzahshop/woocommerce/emails/customer-membership-change.php
+++ wp-content/themes/hamzahshop/woocommerce/emails/customer-membership-change.php
@@ -6,9 +6,9 @@ do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
 <p><?php printf( esc_html__( 'Hi %s,', 'woocommerce' ), esc_html( $first_name) ); ?></p>
 <?php 
-echo "Die Zugangsmethode zum Kurs <strong>BOOST LA POUSSE DE TES CHEVEUX</strong> hat sich geändert. 
-Ab jetzt müssen sie sich über diese Adresse einloggen: " . make_clickable( esc_url( wc_get_page_permalink( 'myaccount' ) ) ) . ". </br>";
-echo  'Dein Username ist <strong>' . esc_html( $user_login ) . '</strong>. ';
-echo "Wenn du dein Paswort vergessen hast, dann kannst du es <a href='" . esc_url($user_login_uri ) .  "' >hier</a> zurücksetzen";
-echo "<p>Liebe Grüße Valérie</p>";
+echo "Le mode d'accès au cours <strong>BOOST LA POUSSE DE TES CHEVEUX</strong> a changé. 
+A partir de maintenant, vous devez vous connecter en utilisant cette adresse : " . make_clickable( esc_url( wc_get_page_permalink( 'myaccount' ) ) ) . ". </br>" ;
+echo 'Votre nom d utilisateur est <strong>' . esc_html( $user_login ) . '</strong>. ' ;
+echo "Si vous avez oublié votre mot de passe, vous pouvez <a href='" . esc_url($user_login_uri ) .  "' > here</a> reset" ;
+echo "<p> Aimer Valérie</p>";
 do_action( 'woocommerce_email_footer', $email );
--- wp-content/themes/hamzahshop/woocommerce/emails/customer-registration.php
+++ wp-content/themes/hamzahshop/woocommerce/emails/customer-registration.php
@@ -6,12 +6,12 @@ do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
 <p><?php printf( esc_html__( 'Hi %s,', 'woocommerce' ), esc_html( $first_name) ); ?></p>
 <p><?php echo  
-    "Die Zugangsmethode zum Kurs <strong>BOOST LA POUSSE DE TES CHEVEUX</strong> hat sich geändert. 
-    Wir haben für dich ein neues Konto eingerichtet. Dein Username ist <strong>" . esc_html( $user_login ) . "</strong>. 
-    Du musst jetzt nur noch dein Passwort ändern. Dafür musst du  <a href='" . esc_url($user_login_uri ) .  "' >hier</a> clicken.
-    Danach kannst du dich einloggen um dein Kurs zu sehen "
+    "Le mode d'accès au cours <strong>BOOST LA POUSSE DE TES CHEVEUX</strong> a changé. 
+    Nous avons créé un nouveau compte pour vous. Votre nom d'utilisateur est <strong>" . esc_html( $user_login ) . "</strong>. 
+    Il ne vous reste plus qu'à changer votre mot de passe. Pour cela, vous avez besoin de <a href='" . esc_url($user_login_uri ) .  "' > ici</a> cliquez.
+    Ensuite, vous pouvez vous connecter pour voir votre cours"
      ?></p>
-     <p>Liebe Grüße Valérie</p>
+     <p>Aimer Valérie</p>
 <?php
 
 do_action( 'woocommerce_email_footer', $email );
