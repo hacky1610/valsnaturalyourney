f880142e587209ecce9379b1cc52321a7b588d94
Daniel <hacky1610@gmx.de>
2020-05-24 21:34:42 +0200

    fix test

--- test/SmokeTest.js
+++ test/SmokeTest.js
@@ -6,7 +6,7 @@ const siteUri = 'https://vals-natural-journey.de';
 let driver =  new Builder().forBrowser('firefox').setFirefoxOptions(new firefox.Options().headless()).build();
 
 driver.get(siteUri).then(function(){
-  driver.findElement(By.className('homebar')).then(function(webElement) {
+  driver.findElement(By.className('home')).then(function(webElement) {
     console.log("Found");
   },
   function(err) {
