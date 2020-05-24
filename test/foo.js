3db3128573286064827589dd140a8a8ebdcd7788
Daniel <hacky1610@gmx.de>
2020-05-24 12:12:31 +0200

    fix npm installation

--- test/foo.js
+++ test/foo.js
@@ -1,7 +1,7 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
  
 (async function example() {
-  let driver = await new Builder().forBrowser('firefox').build();
+  let driver = await new Builder().forBrowser('chrome').build();
   try {
     await driver.get('https://vals-natural-journey.de/');
     await driver.wait(until.titleIs('Accueil - Vals Natural Journe'), 1000);
