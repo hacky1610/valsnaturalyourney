66e45c3f83a09f961c6c0b1b1fd3269c65e4e93e
Daniel <hacky1610@gmx.de>
2020-05-24 21:20:26 +0200

    Refactor test

---
+++ test/SmokeTest.js
@@ -0,0 +1,17 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const firefox = require('selenium-webdriver/firefox');
+//const siteUri = 'http://localhost';
+const siteUri = 'https://vals-natural-journey.de';
+
+let driver =  new Builder().forBrowser('firefox').setFirefoxOptions(new firefox.Options().headless()).build();
+
+driver.get(siteUri).then(function(){
+  driver.findElement(By.className('homebar')).then(function(webElement) {
+    console.log("Found");
+  },
+  function(err) {
+      console.log(err);
+      throw err;
+  }
+  );
+});
