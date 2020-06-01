74ae458b74868d091485ceb3aaa5da1fab7cbccd
hacky1610@gmx.de <hacky1610@gmx.de>
2020-06-01 20:53:16 +0200

    Add tests

--- test/SmokeTest.js
+++ test/SmokeTest.js
@@ -1,17 +1,27 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
-//const siteUri = 'http://localhost';
+var assert = require('assert');//const siteUri = 'http://localhost';
 const siteUri = 'https://vals-natural-journey.de';
-
 let driver =  new Builder().forBrowser('firefox').setFirefoxOptions(new firefox.Options().headless()).build();
 
-driver.get(siteUri).then(function(){
-  driver.findElement(By.className('home')).then(function(webElement) {
-    console.log("Found");
-  },
-  function(err) {
-      console.log(err);
-      process.exit(-1);
-  }
-  );
+
+describe('Website', function() {
+  describe('Homepage', function() {
+    it('can be loaded', async () => {
+      await driver.get(siteUri);
+      let home = await driver.findElement(By.className('home'))
+      assert.ok(true);
+  });
 });
+
+  describe('Product page', function() {
+    it('can be loaded', async () => {
+      await driver.get(siteUri + "/product/la-recette-pour-des-cheveux-long-foo/");
+      let home = await driver.findElement(By.className('single_add_to_cart_button'))
+      assert.ok(true);
+  });
+
+
+  });
+});
+
