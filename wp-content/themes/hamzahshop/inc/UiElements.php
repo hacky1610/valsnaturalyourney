0552bea45d03d533254bd7519a3a90fd6f5f8946
Daniel <hacky1610@gmx.de>
2021-01-01 20:03:43 +0100

    Format

--- wp-content/themes/hamzahshop/inc/UiElements.php
+++ wp-content/themes/hamzahshop/inc/UiElements.php
@@ -1,23 +1,25 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-class UiElements {
+class UiElements
+{
 
     private $siteUri;
-    function __construct(){
+    public function __construct()
+    {
         $this->siteUri = get_site_url();
     }
 
-    function ShowAccountButton()
+    public function ShowAccountButton()
     {
         echo '<div class="sharonne_account_container">';
         $myAccountUri = $this->siteUri . "/mon-compte";
-        if (is_user_logged_in()) { ?>
+        if (is_user_logged_in()) {?>
         <div class="btn-group">
             <button class="btn dropdown-toggle user-button" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="fas fa-user"></span>
@@ -28,7 +30,7 @@ class UiElements {
                 <li><a href="<?php echo wp_logout_url(get_permalink()); ?>">Se déconnecter</a></li>
             </ul>
         </div>
-        <?php } else { ?>
+        <?php } else {?>
         <div class="btn-group">
             <button class="btn dropdown-toggle user-button" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <span class="fas fa-user"></span>
@@ -39,16 +41,17 @@ class UiElements {
         </div>
         <?php
         }
+        echo '</div>';
+
 
     }
 
-    function getCourseOverViewUri()
+    public function getCourseOverViewUri()
     {
-        return  $this->siteUri . "/mon-compte/members-area";
+        return $this->siteUri . "/mon-compte/members-area";
     }
 
-    
-    function vnj_checkout_register()
+    public function vnj_checkout_register()
     {?>
         <p class="form-row form-row-wide create-account woocommerce-validated">
             <label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
@@ -56,34 +59,32 @@ class UiElements {
             </label>
         </p>
     <?php
-    }
+}
 
-    function getCart($woocommerCart)
-    {?>
+    public function getCart($woocommerCart)
+    {
+        ?>
         <ul class="header-r-cart">
             <li>
                 <a class="cart" href="<?php echo esc_url(wc_get_cart_url()); ?>">
                     <?php
-                    $count = esc_attr($woocommerCart->get_cart_contents_count());
-                    if ($count > 0) {
-                        echo $count;
-                    }
-                    ?>
+$count = esc_attr($woocommerCart->get_cart_contents_count());
+        if ($count > 0) {
+            echo $count;
+        }
+        ?>
                 </a>
             </li>
         </ul>
     <?php
-    }
+}
 
-    function ShowShakeComment($cartArray)
+    public function ShowShakeComment($cartArray)
     {
-        foreach ( $cartArray as $cart_item_key => $cart_item ) 
-        {
+        foreach ($cartArray as $cart_item_key => $cart_item) {
             $arr = $cart_item["data"]->category_ids;
-            for($i = 0; $i < count($arr); $i++)
-            {
-                if($arr[$i]== 170)
-                {
+            for ($i = 0; $i < count($arr); $i++) {
+                if ($arr[$i] == 170) {
                     echo "<div class='alert alert-success' role='alert'>Vous voulez une dédicace pour votre enfant? Veuillez laisser son nom et votre message en commentaire.</div>";
                     return;
                 }
@@ -91,10 +92,9 @@ class UiElements {
         }
     }
 
-    function CreateCourseButton($userStartDate, $postId, $name, $days,$dateCurrent = Null)
+    public function CreateCourseButton($userStartDate, $postId, $name, $days, $dateCurrent = null)
     {
-        if(!isset($dateCurrent))
-        {
+        if (!isset($dateCurrent)) {
             $dateCurrent = new DateTime();
         }
 
@@ -102,18 +102,18 @@ class UiElements {
 
         $interval = $dateCurrent->diff($userStartDate);
 
-        $class = "btn btn-primary btn-lg course-button";
+        $class  = "btn btn-primary btn-lg course-button";
         $option = "";
-        $name = "<h1>$name</h1>";
-        $i = new DateInterval("P" . $days . "D");
+        $name   = "<h1>$name</h1>";
+        $i      = new DateInterval("P" . $days . "D");
         $userStartDate->add($i);
         if ($dateCurrent->getTimestamp() < $userStartDate->getTimestamp()) {
             $timeToCourse = ($dateCurrent->diff($userStartDate))->days;
             $class .= " disabled course-button-disabled";
             $option = "aria-disabled='true'";
-            $name = "<h1>$name</h1><p>Disponible en $timeToCourse jours</p>";
+            $name   = "<h1>$name</h1><p>Disponible en $timeToCourse jours</p>";
         }
 
-        return  sprintf("<a  href='%1\$s' target='_blank' class='%3\$s' role='button' %4\$s>%2\$s</a>", $postUri, $name, $class, $option);
+        return sprintf("<a  href='%1\$s' target='_blank' class='%3\$s' role='button' %4\$s>%2\$s</a>", $postUri, $name, $class, $option);
     }
 }
